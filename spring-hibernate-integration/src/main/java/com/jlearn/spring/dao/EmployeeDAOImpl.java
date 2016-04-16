3df8c8e4eacdf5e1974c59f8aa37bb0b30e29cba
Sriram <hamza@hamza-PC>
2016-04-16 14:26:29 -0700

    Spring Hibernate Integration

---
+++ spring-hibernate-integration/src/main/java/com/jlearn/spring/dao/EmployeeDAOImpl.java
@@ -0,0 +1,35 @@
+package com.jlearn.spring.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.jlearn.spring.dos.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void save(Employee e) {
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(e);
+		tx.commit();
+		session.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Employee> list() {
+		Session session = this.sessionFactory.openSession();
+		List<Employee> EmployeeList = session.createQuery("from Employee").list();
+		session.close();
+		return EmployeeList;
+	}
+
+}
